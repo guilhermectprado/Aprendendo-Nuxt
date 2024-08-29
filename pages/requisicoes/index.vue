856c0a593c0072b3b8fa9076404706b200f163e9
guilhermectprado <guilhermectprado@gmail.com>
2024-08-28 20:08:30 -0400

    paginas de requisicoes

---
+++ pages/requisicoes/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    <li>
+      <NuxtLink to="/useFetch"> useFetch </NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/useAsyncData"> useAsyncData </NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/useAsyncData"> OFetch </NuxtLink>
+    </li>
+  </ul>
+</template>
